5c5e89b71fd7b0ba91a0647ccc15dff66304a1a3
maxam2017 <whk0313@gmail.com>
2022-06-07 21:23:48 +0800

    feat: implement list view

--- src/ListView.svelte
+++ src/ListView.svelte
@@ -1,2 +1,38 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { intersection } from './actions';
+
+	type T = $$Generic;
+
+	interface $$Slots {
+		default: {
+			item: T;
+		};
+	}
+
+	// props
+	export let component: string = 'div';
+	export let items: T[] = [];
+
+	// local state
+	let rest: T[] = [];
+	let lastItem: T;
+	$: {
+		rest = items.slice(0, -1);
+		lastItem = items[items.length - 1];
+	}
+
+	const dispatch = createEventDispatcher();
+	const handleIntersection = () => dispatch('loadmore');
 </script>
+
+<svelte:element this={component}>
+	{#each rest as item}
+		<slot {item} />
+	{/each}
+	{#if lastItem}
+		<div use:intersection on:intersection={handleIntersection}>
+			<slot item={lastItem} />
+		</div>
+	{/if}
+</svelte:element>
